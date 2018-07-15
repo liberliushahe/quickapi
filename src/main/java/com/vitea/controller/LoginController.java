a8d7a3e2eddc899d6e556431a4db595431f6d4b0
liberliushahe <32258497+liberliushahe@users.noreply.github.com>
2018-07-15 16:29:33 +0800

    登录控制器

--- src/main/java/com/vitea/controller/LoginController.java
+++ src/main/java/com/vitea/controller/LoginController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
-	@RequestMapping("index.do")
+	@RequestMapping("login.do")
 	public String getIndex(){
-		return "index";
+		return "login";
 	}
 }
