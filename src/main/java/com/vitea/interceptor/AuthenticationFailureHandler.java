f4c4b6bc3a238ff78486f557587b28ac3d889a5b
liberliushahe <32258497+liberliushahe@users.noreply.github.com>
2018-07-20 17:37:04 +0800

    用户登录拦截器

---
+++ src/main/java/com/vitea/interceptor/AuthenticationFailureHandler.java
@@ -0,0 +1,37 @@
+package com.vitea.interceptor;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+import com.vitea.util.IpAddressUtil;
+/**
+ * 拦截失败请求
+ * @author liushahe
+ *
+ */
+public class AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+	private static Logger logger = LoggerFactory.getLogger(UserAuthenticationSuccessHandler.class);
+
+	public AuthenticationFailureHandler(){
+    	this.setDefaultFailureUrl("/login.html?error=true");
+    }
+	@Override
+	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException exception) throws IOException, ServletException {
+		    logger.info("认证失败信息:{},时间:{},ip:{}", new Object[] {exception.getMessage(),new Date(),IpAddressUtil.getIpAddress(request)});
+		    super.onAuthenticationFailure(request, response, exception);
+	}
+	
+	
+	
+
+}
---
+++ src/main/java/com/vitea/interceptor/UserAuthenticationSuccessHandler.java
@@ -0,0 +1,70 @@
+package com.vitea.interceptor;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import com.vitea.dao.IUserLogDao;
+import com.vitea.domain.User;
+import com.vitea.domain.Userlog;
+import com.vitea.util.DateFormatUtil;
+import com.vitea.util.IpAddressUtil;
+/**
+ * 用户成功登陆后执行操作
+ * 记录登录日志
+ * @author liushahe
+ *
+ */
+@Component
+public class UserAuthenticationSuccessHandler implements AuthenticationSuccessHandler{
+    @Autowired
+	IUserLogDao iUserLogDao;
+	private static Logger logger = LoggerFactory.getLogger(UserAuthenticationSuccessHandler.class);
+    //登录成功跳转url
+	private String defaultTargetUrl;
+
+	//是否重定向
+	private boolean forwardToDestination = false;
+	
+	private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException {
+		//保存登录信息
+		this.saveUserLoginInfo(request, authentication);
+		if(this.forwardToDestination) {
+			request.getRequestDispatcher(this.defaultTargetUrl).forward(request, response);
+		}else {
+			this.redirectStrategy.sendRedirect(request, response, this.defaultTargetUrl);
+		}
+	}
+
+    public void saveUserLoginInfo(HttpServletRequest request,Authentication authentication) {
+    	User user = (User)authentication.getPrincipal();	
+		String ip =IpAddressUtil.getIpAddress(request);
+		Userlog userlog=new Userlog();
+		userlog.setId(2);
+		userlog.setIp(ip);
+		userlog.setLatesttime(DateFormatUtil.getFormatDate());
+		userlog.setUsername(user.getUsername());
+		logger.info("登录用户名{},登录ip:{},登录时间:{}", new Object[]{user.getUsername(),ip,DateFormatUtil.getFormatDate()});
+		iUserLogDao.insert(userlog);  	
+    }
+	public void setDefaultTargetUrl(String defaultTargetUrl) {
+		this.defaultTargetUrl = defaultTargetUrl;
+	}
+	public void setForwardToDestination(boolean forwardToDestination) {
+		this.forwardToDestination = forwardToDestination;
+	}
+
+    
+}
