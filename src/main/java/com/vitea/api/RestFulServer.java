65e5275c69c9c5f9f6fc003345e2c00b5cbd2134
liberliushahe <32258497+liberliushahe@users.noreply.github.com>
2018-07-23 17:23:45 +0800

    优化功能

--- src/main/java/com/vitea/api/RestFulServer.java
+++ src/main/java/com/vitea/api/RestFulServer.java
@@ -1,6 +1,15 @@
 package com.vitea.api;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.vitea.domain.User;
 
 /**
  * 对外提供API服务
@@ -9,6 +18,16 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class RestFulServer {
+	Map<String,String> map=new HashMap<String,String>();
 	
+	@RequestMapping(value = "/getsheet/{id}", method = RequestMethod.GET)
+    public @ResponseBody User sayHello(@PathVariable String id) {
+		
+		User u=new User();
+		u.setEmail("11111");
+		u.setUrl("http:kkkkk");
+        return u;
+    }
+
 
 }
