b4525a67c0ffdda95cfcc581c427f78e2325503c
liberliushahe <32258497+liberliushahe@users.noreply.github.com>
2018-07-26 11:34:06 +0800

    新增测试用例

---
+++ src/test/java/com/vitea/util/FormatDateTest.java
@@ -0,0 +1,15 @@
+package com.vitea.util;
+
+import org.junit.Test;
+/**
+ * 
+ * @author liushahe
+ *
+ */
+public class FormatDateTest {
+	@Test
+	public void getTimeMill() {
+		System.out.println(DateFormatUtil.getFormatDateMill());
+	}
+
+}
