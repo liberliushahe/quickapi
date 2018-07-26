98adb54b6db2868e4639ce3374cb9e613a12b4aa
liberliushahe <32258497+liberliushahe@users.noreply.github.com>
2018-07-26 11:01:01 +0800

    优化功能

--- src/main/java/com/vitea/util/ConstantsUtil.java
+++ src/main/java/com/vitea/util/ConstantsUtil.java
@@ -5,5 +5,28 @@ package com.vitea.util;
  *
  */
 public class ConstantsUtil {
+	/**
+     * 性别
+     */
+    public static enum sex{
+        
+        MAN("1","男"),FEMAN("2","女");
+        
+        private sex(String value,String name){
+            this.value = value;
+            this.name = name;
+        }
+        private final String value;
+        private final String name;
+        
+        public String getValue() {
+            return value;
+        }
+
+        public String getName() {
+            return name;
+        }
+        
+    }
 
 }
