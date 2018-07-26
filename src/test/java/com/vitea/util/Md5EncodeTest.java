d75d54a70306ff5ec37309e10c8367a07a964a15
liberliushahe <32258497+liberliushahe@users.noreply.github.com>
2018-07-26 11:33:40 +0800

    优化测试类

--- src/test/java/com/vitea/util/Md5EncodeTest.java
+++ src/test/java/com/vitea/util/Md5EncodeTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
  * @author liushahe
  *
  */
-public class MD5EncodeTest {
+public class Md5EncodeTest {
 	@Test
 	public void encode() {
 		String pass=Md5PassWordEncoder.getMD5PasswordEncoder().encode("admin");
