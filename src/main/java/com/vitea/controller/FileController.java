036da93c6ba18b6587aad30597a5e256e33f2430
liberliushahe <32258497+liberliushahe@users.noreply.github.com>
2018-07-30 18:08:59 +0800

    文件上传控制器

---
+++ src/main/java/com/vitea/controller/FileController.java
@@ -0,0 +1,17 @@
+package com.vitea.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+/**
+ * 文件上传控制器
+ * @author liushahe
+ *
+ */
+@Controller
+public class FileController {
+	@RequestMapping("upload")
+	public void fileUpload() {
+		
+	}
+
+}
