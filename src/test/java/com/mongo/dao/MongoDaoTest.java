6cef8642aec5e212dc25aa88f650a63e878ce0ab
liberliushahe <32258497+liberliushahe@users.noreply.github.com>
2018-08-06 10:48:47 +0800

    新增测试用例

---
+++ src/test/java/com/mongo/dao/MongoDaoTest.java
@@ -0,0 +1,37 @@
+package com.mongo.dao;
+
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.mongo.domain.InterfaceLog;
+import com.vitea.domain.User;
+
+
+/**
+ * 
+ * @author liushahe
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/spring-mongo.xml")
+public class MongoDaoTest {
+	@Autowired
+	protected InterfaceLogDao interfaceLogDao;
+	@Test
+	public void getCollectionSize(){
+		List<InterfaceLog> list=interfaceLogDao.findForRequery("18919927184");
+		System.out.println(list.size());
+	 
+     
+		
+	}
+}
