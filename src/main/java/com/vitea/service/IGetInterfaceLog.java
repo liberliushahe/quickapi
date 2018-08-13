6850446005ee4e2ab13f79b012433cae24021a5e
liberliushahe <32258497+liberliushahe@users.noreply.github.com>
2018-08-13 14:44:54 +0800

    优化功能

--- src/main/java/com/vitea/service/IGetInterfaceLog.java
+++ src/main/java/com/vitea/service/IGetInterfaceLog.java
@@ -10,15 +10,22 @@ import com.mongo.domain.InterfaceLog;
  *
  */
 public interface IGetInterfaceLog {
-	/**
-	 * 通过主键获取日志信息
-	 * @param key
-	 * @return
-	 */
+     /**
+      * 通过主键获取日志信息
+      * @param start
+      * @param end
+      * @param accnum
+      * @param index
+      * @param size
+      * @return
+      */
 	public List<InterfaceLog> getInterfaceLogByKey(String start, String end, String accnum, int index, int size);
-	/**
-	 * 查询总记录
-	 * @return
-	 */
+     /**
+      * 查询总记录
+      * @param start
+      * @param end
+      * @param accnum
+      * @return
+      */
 	public long findAllCount(String start,String end,String accnum);
 }
