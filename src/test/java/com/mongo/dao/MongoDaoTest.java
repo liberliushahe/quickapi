27011bbd6835bbc0fcb48eab6a492d5f9c500bce
liberliushahe <32258497+liberliushahe@users.noreply.github.com>
2018-08-13 14:46:24 +0800

    优化测试用例

--- src/test/java/com/mongo/dao/MongoDaoTest.java
+++ src/test/java/com/mongo/dao/MongoDaoTest.java
@@ -1,11 +1,19 @@
 package com.mongo.dao;
 
 
+import java.util.Iterator;
 import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import com.mongo.domain.InterfaceLog;
@@ -20,12 +28,31 @@ import com.mongo.domain.InterfaceLog;
 public class MongoDaoTest {
 	@Autowired
 	protected InterfaceLogDao interfaceLogDao;
+    @Autowired
+    MongoTemplate mongoTemplate;
+	
 	@Test
-	public void getCollectionSize(){
-		List<InterfaceLog> list=interfaceLogDao.findForRequery("18919927184");
-		System.out.println(list.size());
-	 
-     
-		
+	public void getCollectionByPage(){
+	    String starttime="2018-08-08 12:00:00";
+	    String endtime="2018-08-08 18:00:00";
+        Query query = new Query();
+        query.addCriteria(Criteria.where("accnum").is("18919927184"));
+        query.addCriteria(Criteria.where("timestamp").gte(starttime).lt(endtime));
+        Pageable pageable = new PageRequest(1, 10); 
+        query.with(pageable);
+        // 排序
+        query.with(new Sort(Direction.ASC,"timestamp"));
+        List<InterfaceLog> list=mongoTemplate.find(query,InterfaceLog.class, "logback");
+		Iterator<InterfaceLog> it=list.iterator();
+	    while(it.hasNext()) {
+	    	System.out.println(it.next().getAccnum());
+	    }	
+	}
+	@Test
+	public void findAllCount() {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("accnum").is("18919927184"));
+		System.out.println(mongoTemplate.count(query, InterfaceLog.class,"logback"));
+	
 	}
 }
