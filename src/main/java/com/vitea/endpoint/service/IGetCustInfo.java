9bbc1ae54311058c590f6610b7cc40ee4198bb9a
liberliushahe <32258497+liberliushahe@users.noreply.github.com>
2018-08-23 10:36:23 +0800

    优化功能

--- src/main/java/com/vitea/endpoint/service/IGetCustInfo.java
+++ src/main/java/com/vitea/endpoint/service/IGetCustInfo.java
@@ -6,9 +6,9 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-
 import com.alibaba.fastjson.JSONObject;
 import com.vitea.endpoint.dto.CustInfo;
+
 /**
  * 查询客户信息接口
  * @author liushahe
@@ -17,8 +17,8 @@ import com.vitea.endpoint.dto.CustInfo;
 @WebService
 public interface IGetCustInfo {
     /**
-     * 客户部分信息restful接口
-     * 接口地址:http://localhost:8888/quickapi/ws/rest/custinfo/1
+     * GET获取客户部分信息restful接口
+     * 接口地址:http://localhost:8888/quickapi/ws/info/custinfo/1
      * @param id
      * @return
      */
@@ -28,13 +28,51 @@ public interface IGetCustInfo {
 	public CustInfo  queryCustInfo(@PathParam("id") String id);
 	
 	/**
-	 * POST获取客户信息
+	 * POST获取详细客户信息
 	 * @param paramString
 	 * @return
 	 */
 	@POST
     @Path("/custinfo")  
 	@Produces({"application/json"}) 
-	public JSONObject queryDetailCustInfoPost(String paramString);
+	public JSONObject queryDetailCustInfo(String paramString);
 	
+	/**
+	 * POST获取用户信息
+	 * @param paramString
+	 * @return
+	 */
+	@POST
+    @Path("/userinfo")  
+	@Produces({"application/json"}) 
+	public JSONObject queryDetailUserInfo(String paramString);
+	
+	/**
+	 * POST获取用户套餐信息
+	 * @param paramString
+	 * @return
+	 */
+	@POST
+    @Path("/userpkgs")  
+	@Produces({"application/json"}) 
+	public JSONObject queryDetailUserPackages(String paramString);
+	/**
+	 * POST获取用户套餐信息
+	 * @param paramString
+	 * @return
+	 */
+	@POST
+    @Path("/prodlist")  
+	@Produces({"application/json"}) 
+	public JSONObject queryDetailProdList(String paramString);
+	
+	/**
+	 * POST获取用户套餐信息
+	 * @param paramString
+	 * @return
+	 */
+	@POST
+    @Path("/subproduct")  
+	@Produces({"application/json"})
+	public JSONObject queryDetailSubProducts(String paramString);
 }
