b7d9429525aa41ab705a248f624dc021ad1ad493
liberliushahe <32258497+liberliushahe@users.noreply.github.com>
2018-08-23 15:51:19 +0800

    新增WSDL实现类

---
+++ src/main/java/com/vitea/endpoint/service/impl/QueryInfoWsdlImpl.java
@@ -0,0 +1,42 @@
+package com.vitea.endpoint.service.impl;
+
+import com.vitea.endpoint.service.IQueryInfoWsdl;
+
+/**
+ * 
+ * @author liushahe
+ *
+ */
+public class QueryInfoWsdlImpl implements IQueryInfoWsdl {
+
+	@Override
+	public String qryCustInfo(String paramString) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String qryUserInfo(String paramString) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String qryUserPackageInfo(String paramString) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String qryMainProductInfo(String paramString) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String qryAuxiliaryProductInfo(String paramString) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
