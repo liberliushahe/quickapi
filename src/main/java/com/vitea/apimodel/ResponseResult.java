1a4bffe5abd20e96a09575418ccc6940af2f0d19
liberliushahe <32258497+liberliushahe@users.noreply.github.com>
2018-08-29 09:01:36 +0800

    响应结果实体

---
+++ src/main/java/com/vitea/apimodel/ResponseResult.java
@@ -0,0 +1,18 @@
+package com.vitea.apimodel;
+/**
+ * 响应结果
+ * @author liushahe
+ *
+ */
+public class ResponseResult {
+   private Object object;
+
+   public Object getObject() {
+	return object;
+}
+
+public void setObject(Object object) {
+	this.object = object;
+}
+
+}
